7d8201ae9729f5f19a0cd8c4970a46cb016dd922
Hectormalbe28 <hectormalbe28@gmail.com>
2025-04-04 13:54:28 -0500

    feat: Ajuste para usar firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCE7yfbgOQohExiW9E-OWICWIw1vyUIgXw",
+    authDomain: "todo-list-de1c5.firebaseapp.com",
+    projectId: "todo-list-de1c5",
+    storageBucket: "todo-list-de1c5.firebasestorage.app",
+    messagingSenderId: "206872190356",
+    appId: "1:206872190356:web:c836485cc7d628984b0381"
+  }
 };
